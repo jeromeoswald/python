d0922832af2a58cb89636205165ba6d44f32fe97
Jerome Oswald Ebenezer J <jeromeoswald7@gmail.com>
2024-03-22 17:15:22 +0530

    Python1

---
+++ Hello World.py
@@ -0,0 +1,47 @@
+"""
+#First Code
+print("Hello, World!")
+
+x= 1 #int
+y= 1.0 #float
+z= 1j #complex
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = float(x)
+b = int(x)
+c=complex(x)
+
+print(a)
+print(b)
+print(c)
+"""
+
+a= 10
+b= 20
+
+#addition
+x=(a+b)
+#subtraction
+y=(a-b)
+#multiple
+z=(a*b)
+#division
+w=(a/b)
+#modulus
+v=(a%b)
+#exponential
+u=(a**b)
+#floordivision
+s=(a//b)
+
+print(x)
+print(y)
+print(z)
+print(w)
+print(v)
+print(u)
+print(s)
