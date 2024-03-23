abac8d057a0eaa11019fb25874ba2160026163a4
jeromeoswald <jeromeoswald7@gmail.com>
2024-03-23 18:36:56 +0530

    Day-2 Python

---
+++ Day-2 Python.py
@@ -0,0 +1,76 @@
+"""
+a= str(10)
+b= int(10)
+c= float(10)
+
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+x,y,z = "Banana", "Apple", "Orange"
+
+print(x)
+print(y)
+print(z)
+
+x=y=z = "Banana"
+
+print(x)
+print(y)
+print(z)
+
+x= '''Oranges 
+are
+Orange in Color'''
+print(x)
+
+
+x= "Hello, World!  "
+#print(x[12])
+
+# for a in x:
+#  print(a)
+#print(len(x))
+#print ("like" in x)
+#print(x[6:13])
+#print(x[-7:-1])
+# print(x.upper())
+# print(x.lower())
+# print(x.replace("H", "Y"))
+# print(x.strip())
+print(x.split(","))
+
+"""
+# #concodinate
+# a ="Hello"
+# b="world"
+# print(a+" "+b)
+
+#conditional statment
+# a==b
+# a!=b
+# a>b
+# a>=b
+# a<b
+# a<=b
+
+a= 150
+b= 99
+c=111
+# if a>b:
+#     print("a is greater than b")
+# elif a==b:
+#     print("a and b are same")
+# else:
+#     print("b is greater than a")
+#print("a is greater than b") if a>b else print("b is greater than a")
+if a>b or c>a:
+    print("a")
+else:
+    print("b")
